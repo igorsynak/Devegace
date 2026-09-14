54d023fd0f9923457bd6fd17c76b7a021f0c62ea
Igor Synak <dla@igorsynak.pl>
2026-09-14 20:45:31 +0200

    Dodanie Discord.net
    Dodanie Discord.net oraz dodanie logów

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Discord;
+
+namespace devegace;
+
+class Program {
+    static async Task Main(String[] argumenty)  { // async oznacza że mogę użyć await ( czekania/wznawiania ) a Task że mogę zwracać future, trch jak w rust
+        Console.WriteLine("witajjj");
+    }
+    private static Task Log(LogMessage wiadomość) { // przyjmuję loga i go printuję, chb prote ale potem dodam zapisy w pliku, wraziewu ( ewoluuje język lol )
+        Console.WriteLine(wiadomość.ToString());
+        return Task.CompletedTask; // imo "Task.CompleteTask" lepsze jest od "return Task.CompleteTask;", rust better xD
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Devagace
-Devagace to opensource'owy projekt bot'a discord, który dodaje kasyno.
+# Devegace
+Devegace to opensource'owy projekt bot'a discord, który dodaje kasyno.
 
-## Po co mi Devagace
+## Po co mi Devegace
 Bro wtf co ty yappujesz kto nie lubi kasyna.
+
+## Komentarze w kodzie
+Komentarze w kodzie często mogą dotyczyć tylko C#, ponieważ tak ja łatwo zapamiętuję/łatwo sobie przypominam co i po co, i tak ten projekt jest tylko dla mnie lol
