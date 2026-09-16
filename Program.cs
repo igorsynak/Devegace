5b2aa9bb9ef2f7802cf740169dca5ca29729226a
Igor Synak <dla@igorsynak.pl>
2026-09-16 20:36:10 +0200

    Łączenie z botem
    Łączenie i logowanie się z botem discord

--- Program.cs
+++ Program.cs
@@ -2,17 +2,19 @@
 using Discord.WebSocket;
 using DotNetEnv;
 
-namespace devegace;
+namespace Devegace;
 
 class Program {
-    private static DiscordSocketClient klient; // chyba nie musi mieć takie nazwy ale lepiej już dam xD, wgl kto wymyślił nazwy zmiennych na końcu jakby wth
-    static string token;
-    public static async Task Main(String[] argumenty)  { // async oznacza że mogę użyć await ( czekania/wznawiania ) a Task że mogę zwracać future, trch jak w rust
-        Env.Load();
-        token = Environment.GetEnvironmentVariable("TOKEN");
-        Console.WriteLine("witajjj");
-        klient = new DiscordSocketClient();
+    private static DiscordSocketClient klient = new(); // chyba nie musi mieć takie nazwy ale lepiej już dam xD, wgl kto wymyślił nazwy zmiennych na końcu jakby wth. New niepotrzebne ale nei chce błędów lol
+    public static async Task Main(String[] argumenty) { // async oznacza że mogę użyć await ( czekania/wznawiania ) a Task że mogę zwracać future, trch jak w rust
+        Env.Load(); // idk pewnie ładuje plik .env do pamięci albo cuś
+        string token = Environment.GetEnvironmentVariable("TOKEN"); // hmmm ciekawe co to robi nwm
+        Console.WriteLine("Witaj");
+        klient = new DiscordSocketClient(); // tworzę klienta
         klient.Log += Dziennik; // += oznacza że jak uruchomi się Log to wykona to co jest po, czyli Dziennik
+        await klient.LoginAsync(TokenType.Bot, token);
+        await klient.StartAsync();
+        await Task.Delay(-1);
     }
     private static Task Dziennik(LogMessage wiadomość) { // przyjmuję loga i go printuję, chb prote ale potem dodam zapisy w pliku, wraziewu ( ewoluuje język lol )
         Console.WriteLine(wiadomość.ToString());
